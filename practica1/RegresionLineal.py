f0cc2fd40d6918f421bc87bb579af1080530abb9
jmplz14 <jmplz14@gmail.com>
2019-03-24 00:01:54 +0100

    Realizado documentacion hasta el ejercicio 1 del segundo apartado

--- practica1/RegresionLineal.py
+++ practica1/RegresionLineal.py
@@ -5,18 +5,26 @@ Created on Wed Mar 20 09:20:22 2019
 
 @author: jose
 """
+import matplotlib.patches as mpatches
 import numpy as np
 import matplotlib.pyplot as plt
 def obtenerValorPesos(punto, pesos):
     return [pesos[0] + pesos[1]*punto[0] + pesos[2]*punto[0], pesos[1] + pesos[1]*punto[1] + pesos[2]*punto[1]]
 
-def dibujarGrafico2D(X, Y, clases, pesos):
+def dibujarGrafico2D(X, Y, clases, pesos, numero, labelx, labely, titulo):
+    plt.figure(numero)
     x = np.linspace(0,0.6,100)
     plt.plot(x,-pesos[0]/pesos[2] - pesos[1]/pesos[2]*x, 'r-') 
+    plt.title(titulo)
     plt.scatter(X,Y, c=clases)
+    plt.xlabel(labelx)
+    plt.ylabel(labely)
+    cinco_patch = mpatches.Patch(color='yellow', label='5')
+    uno_patch = mpatches.Patch(color='purple', label='1')
+
+    plt.legend(handles=[cinco_patch,uno_patch])
     plt.show()
     
-
 def obtenerDatos(ficheroTraining, ficheroTest):
 
     datos_training = np.load(ficheroTraining)
@@ -47,18 +55,18 @@ def obtenerDatos(ficheroTraining, ficheroTest):
 def pseudoInversa(x,y):
     return np.linalg.inv(x.T @ x) @ x.T @ y 
 
-def h(x_, w_):
-    sol = np.zeros(len(x_),np.float64)
+def h(x, pesos):
+    solucion = np.zeros(len(x),np.float64)
     i = 0
-    for i in range(len(sol)):
-        sol[i] = np.dot(np.transpose(w_),x_[i])
+    for i in range(len(solucion)):
+        solucion[i] = np.dot(np.transpose(pesos),x[i])
         
-    return sol
+    return solucion
    
     
     
     
-def gradienteDescendenteEstocastico(x, y,num_iteraciones,tam_minibatch,tasa_aprendizaje):
+def gradienteDescendenteEstocastico(x, y, x_test, y_test, num_iteraciones,tam_minibatch,tasa_aprendizaje):
     pesos = np.zeros(x[0].size)
     tamano_muestra = y.size
     print(y.size)
@@ -66,7 +74,8 @@ def gradienteDescendenteEstocastico(x, y,num_iteraciones,tam_minibatch,tasa_apre
     pos_permutacion = 0
     pos_minibatch = 0
     iteraciones = 0
-    
+    mejor_error = 10000
+    mejor_pesos = None
     x_minibatch = np.empty((tam_minibatch,3))
     y_minibatch = np.empty(tam_minibatch)
     while iteraciones < num_iteraciones:
@@ -76,6 +85,9 @@ def gradienteDescendenteEstocastico(x, y,num_iteraciones,tam_minibatch,tasa_apre
             suma += np.dot(np.transpose(x_minibatch),( h(x_minibatch,pesos)-y_minibatch ))
             pesos=pesos-tasa_aprendizaje*(2.0/tam_minibatch)*suma
             iteraciones += 1
+            error = error_out(x_test,y_test,pesos)
+            if mejor_error > error:
+                mejor_pesos = pesos
             x_minibatch = np.empty((tam_minibatch,3))
             y_minibatch = np.empty(tam_minibatch)
             #pesos = calcularPesos(x_minibatch, pesos);
@@ -99,12 +111,20 @@ def gradienteDescendenteEstocastico(x, y,num_iteraciones,tam_minibatch,tasa_apre
         pos_permutacion += 1
         pos_minibatch += 1
         
-    return pesos
-        
-    
-        
-    
-    
+    return mejor_pesos
+       
+def error(x,y,pesos):
+    return (1/y.size)*(pesos.T @ x.T @ x @ pesos - 2*pesos.T @ x.T @ y + y.T @ y )
+
+"""def error_out(x,y,pesos):
+    num_errores = 0
+    for i in range(y.size):
+        resultado = np.dot(x[i],pesos)
+        if(resultado < 0 and y[i] == 1): 
+            num_errores += 1
+        if(resultado > 0 and y[i] == -1):
+            num_errores += 1
+    return 1.0*num_errores/y.size"""
     
     
     
@@ -115,15 +135,24 @@ def main():
     test, clases_test = obtenerDatos("datos/X_test.npy", "datos/y_test.npy")
     
     
-    
+    gde = gradienteDescendenteEstocastico(training, clases_training,test, clases_test, 100,128,0.1)
     Inversa = pseudoInversa(training, clases_training)
+    print("Ejercicios 1")
+    print("El vector de pesos dado por la pseudoinversa es: ", Inversa)
+    print("El vector de pesos dado por el gradiente descendente estocasito es: ", gde)
+    print("El error en el text de la pseudoinversa es: ", error(test, clases_test, Inversa))
+    print("El error en el text del gradiente descendente estocastico es: ", error(test, clases_test, gde))
+    print("El error en el training de la pseudoinversa es: ", error(training, clases_training, Inversa))
+    print("El error en el training del gradiente descendente estocastico es: ", error(training, clases_training, gde))
     
     
     #dibujarGrafico2D(training[: , 1], training[: , 2], clases_training, Inversa)
     
-    pesos = gradienteDescendenteEstocastico(training, clases_training,1000,64,0.01)
+
     
-    dibujarGrafico2D(training[: , 1], training[: , 2], clases_training, pesos)
+    dibujarGrafico2D(training[: , 1], training[: , 2], clases_training, gde, 1, "Intensidad promedio", "Simetria", "Gradiente Descendente Estocastico")
+    dibujarGrafico2D(training[: , 1], training[: , 2], clases_training, Inversa, 2, "Intensidad promedio", "Simetria", "Pseudoinversa")
+    
 
 if __name__== "__main__":
   main()
--- practica1/gradienteDescendente.py
+++ practica1/gradienteDescendente.py
@@ -5,7 +5,7 @@ Created on Wed Mar 13 18:23:48 2019
 
 @author: jose
 """
-
+import matplotlib.patches as mpatches
 import numpy as np
 import sympy as sym
 import matplotlib.pyplot as plt
@@ -52,8 +52,10 @@ from sympy.utilities.lambdify import lambdify, implemented_function
 """  
 
 def gradiante(der_parcial_u,der_parcial_v, u, v, tasaAprendizaje, coordenada):
-    valor_u = coordenada[0] - tasaAprendizaje * np.float64(der_parcial_u.subs({u:coordenada[0], v: coordenada[1]}))
-    valor_v = coordenada[1] - tasaAprendizaje * np.float64(der_parcial_v.subs({u:coordenada[0], v: coordenada[1]}))
+    valor_u = coordenada[0] - tasaAprendizaje \
+    * np.float64(der_parcial_u.subs({u:coordenada[0], v: coordenada[1]}))
+    valor_v = coordenada[1] - tasaAprendizaje \
+    * np.float64(der_parcial_v.subs({u:coordenada[0], v: coordenada[1]}))
     return valor_u,valor_v
 
 def ejercicio2(inicio: list, tasaAprendizaje: np.float64(),lam_formula, formula, u, v,num_iteraciones, minimo):
@@ -70,12 +72,16 @@ def ejercicio2(inicio: list, tasaAprendizaje: np.float64(),lam_formula, formula,
     datos[0][2] = lam_formula(inicio[0],inicio[1])
     i = 1;
     valor_u,valor_v = gradiante(der_parcial_u, der_parcial_v, u, v, tasaAprendizaje,inicio)
+    print("Apartado1")
+    print("Ejercicio 1")
+    print("Valores para E(u,v) = (1,1) = (",valor_u,",",valor_v,")")
     while lam_formula(valor_u,valor_v) > minimo and i < num_iteraciones:
         datos[i][0] = valor_u
         datos[i][1] = valor_v
         datos[i][2] = lam_formula(valor_u,valor_v)
         
         valor_u,valor_v = gradiante(der_parcial_u, der_parcial_v, u, v, tasaAprendizaje,[valor_u,valor_v])
+        
         i = i + 1
         
     if i < num_iteraciones:
@@ -83,6 +89,7 @@ def ejercicio2(inicio: list, tasaAprendizaje: np.float64(),lam_formula, formula,
         datos[i][1] = valor_v
         datos[i][2] = lam_formula(valor_u,valor_v)
         datos.resize(i+1,3)
+        
     
     return datos
 
@@ -107,10 +114,10 @@ def ejercicio3(inicio: list, tasaAprendizaje: np.float64(),lam_formula, formula,
     
     return datos
     
-def dibujarGrafica(datos,rango_u,rango_v,num_muestras, lam_formula):
+def dibujarGrafica(datos,rango_u,rango_v,num_muestras, lam_formula,num, labelx, labely, titulo):
     
 
-
+    plt.figure(num)
     generar_u = np.linspace(rango_u[0], rango_u[1], num_muestras)
     generar_v = np.linspace(rango_v[0], rango_v[1], num_muestras)
     generar_z =  np.zeros((num_muestras, num_muestras))
@@ -125,16 +132,36 @@ def dibujarGrafica(datos,rango_u,rango_v,num_muestras, lam_formula):
         plt.plot(datos[i][0], datos[i][1], "o", c="red")
     
     plt.plot(datos[tamano][0], datos[tamano][1], "o", c="green")
+    plt.title(titulo)
+    plt.xlabel(labelx)
+    plt.ylabel(labely)
+    
+    blue_patch = mpatches.Patch(color='white', label='Punto Inicial')
+    green_patch = mpatches.Patch(color='red', label='Puntos Intermedios')
+    red_patch = mpatches.Patch(color='green', label='Punto Final')
+    
+    #añadimos al legend los distintos tipos
+    plt.legend(handles=[blue_patch,green_patch,red_patch])
+    
+    
     
  
     plt.contourf(generar_u, generar_v, generar_z, num_muestras)
     plt.colorbar()
     plt.show()
-        
-    
-    
-    
-    
+   
+def obtenerMenor(datos):
+    x_menor = datos[0][0]
+    y_menor = datos[0][1]
+    z_menor = datos[0][2]
+    for i in range(1,49):
+        if z_menor > datos[i][2]:
+            x_menor = datos[i][0]
+            y_menor = datos[i][1]
+            z_menor = datos[i][2]
+            
+    print("(", x_menor,",",y_menor, ") =", z_menor)   
+            
     
 def main():
 
@@ -146,8 +173,12 @@ def main():
     formula_ejer2 = (u**2 * sym.exp(v) - 2*v**2*sym.exp(-u))**2
     #formula = u**2 + v**2
     datos_ejer2 = ejercicio2([1,1], 0.01,lam_formula_ejer2, formula_ejer2, u, v, 50, np.float64(1e-14))
-    print(datos_ejer2)
-    dibujarGrafica(datos_ejer2, [0.6,1], [0.9,1], 100, lam_formula_ejer2)
+    print("\nEjercicio 2.a")
+    print("Encuentra un valor menor a 10^-14 en: ", int(datos_ejer2.size/3), "iteraciones contando la de (1,1)" )
+    print("Se encuentran en el punto (",datos_ejer2[-1][0],",",datos_ejer2[-1][1],")")
+    
+    dibujarGrafica(datos_ejer2, [0.6,1.1], [0.9,1.1], 100, lam_formula_ejer2,1,"u","v","Ejercicio 2")
+    
     
     f3 = implemented_function('f', lambda k,n: np.float64((k**2 + 2*n**2 + 2*sym.sin(2*np.pi*k) * sym.sin(2*np.pi*n))))
     lam_formula_ejer3 = lambdify((k,n), f3(k,n))
@@ -157,11 +188,27 @@ def main():
     formula_ejer3 = x**2 + 2*y**2 + 2*sym.sin(2*np.pi*x) * sym.sin(2*np.pi*y)
     
     datos_ejer3_a_1 = ejercicio3([0.1,0.1], 0.01,lam_formula_ejer3, formula_ejer3, x, y, 50)
-    dibujarGrafica(datos_ejer3_a_1, [-0.6,1.01], [-0.5,0.4], 100, lam_formula_ejer3)
-    dibujarGrafica(datos_ejer3_a_1, [-2,2.5], [-2,2], 100, lam_formula_ejer3)
+    dibujarGrafica(datos_ejer3_a_1, [-0.6,1.01], [-0.5,0.4], 100, lam_formula_ejer3,2,"x","y","Ejercicio 3.a 0.01 tasa de aprendizaje")
+    #dibujarGrafica(datos_ejer3_a_1, [-2,2.5], [-2,2], 100, lam_formula_ejer3,3,"x","y","Ejercicio 3.a 0.01 tasa de aprendizaje")
     
     datos_ejer3_a_2 = ejercicio3([0.1,0.1], 0.1,lam_formula_ejer3, formula_ejer3, x, y, 50)
-    dibujarGrafica(datos_ejer3_a_2, [-2,2.5], [-2,2], 100, lam_formula_ejer3)
+    dibujarGrafica(datos_ejer3_a_2, [-2,2.5], [-2,2], 100, lam_formula_ejer3,4,"x","y","Ejercicio 3.a 0.1 tasa de aprendizaje")
+    inicio1 = ejercicio3([1,1], 0.01,lam_formula_ejer3, formula_ejer3, x, y, 50)
+    inicio2 = ejercicio3([-0.5,-0.5], 0.01,lam_formula_ejer3, formula_ejer3, x, y, 50)
+    inicio3 = ejercicio3([-1,-1], 0.01,lam_formula_ejer3, formula_ejer3, x, y, 50)
+    
+    print("\nEjercicio 3 b")
+    print("Empieza en (0.1,0.1)")
+    obtenerMenor(datos_ejer3_a_1)
+    print("Empieza en (1,1)")
+    obtenerMenor(inicio1)
+    print("Empieza en (-0.5,-0.5)" )
+    obtenerMenor(inicio2)
+    print("Empieza en (-1,-1)" )
+    obtenerMenor(inicio3)
+
+    
+    print()
     
     
 if __name__== "__main__":
