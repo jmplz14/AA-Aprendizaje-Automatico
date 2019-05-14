2e35cebf4c5b6b097aca04a0a15358ee3f3fb527
jmplz14 <jmplz14@gmail.com>
2019-05-14 23:49:52 +0200

    Relizado hasta el punto 2 del primer dataset

--- practicas/practica3/practica3.py
+++ practicas/practica3/practica3.py
@@ -5,17 +5,33 @@ Created on Tue May 14 12:23:46 2019
 
 @author: jose
 """
-from numpy import genfromtxt
+from sklearn.feature_selection import VarianceThreshold 
 import numpy as np
 
+
 def normalizarDatos(datos):
+    normalizada = datos / 16
+    return normalizada
+
+def eliminarVarianza(train_X,test_X,limite = 0):
+    row_train = np.size(train_X,0)
     
+    datos = np.concatenate((train_X, test_X), axis=0)
+    
+    selector = VarianceThreshold(limite)
+    datos_procesados = selector.fit_transform(datos)
+    print(selector.get_support())
+    train = datos_procesados[:row_train, :]
+    test = datos_procesados[row_train:, :]
+    return train,test
+
 
 def loadCSV(fichero):
     
-    my_data = genfromtxt(fichero, delimiter=',')
+    my_data = np.genfromtxt(fichero, delimiter=',')
     clases = my_data[:, -1] 
     datos = my_data[:, :-1]
+    #datos = normalizarDatos(datos)
     
     return datos,clases
 
@@ -24,7 +40,16 @@ def loadCSV(fichero):
 
 def main():
     train_X, train_y = loadCSV("datos/optdigits.tra")
-    test_X, text_y = loadCSV("datos/optdigits.tes")
+    test_X, test_y = loadCSV("datos/optdigits.tes")
+    
+    train_X = normalizarDatos(train_X)   
+    test_X = normalizarDatos(test_X)
+    
+    train_X,test_y = eliminarVarianza(train_X,test_X, 0.001);
+    print(np.size(train_X,1))
+    
+    
+    
  
     
 if __name__== "__main__":
